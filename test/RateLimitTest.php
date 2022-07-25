68090c6df165211b7155e23517a3a429ead9434c
Marco Pivetta <ocramius@gmail.com>
2022-07-25 11:42:51 +0200

    Applied automatic CS fixes, prevent `declare(strict_types=1);` on sources (for now)

--- test/RateLimitTest.php
+++ test/RateLimitTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LaminasTest\Twitter;
 
 use Laminas\Http\Header\HeaderInterface;
--- test/_files/generate_data.php
+++ test/_files/generate_data.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Laminas\Http\Client\Adapter\Curl;
 use Laminas\Twitter\Twitter;
 
